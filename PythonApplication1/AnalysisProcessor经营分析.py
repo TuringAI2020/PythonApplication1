564f78ccb226739365eaf425a1cdae3c73a46d6c
TuringAI2020 <turingai@qq.com>
2020-02-04 20:13:54 +0800

    添加经营分析和融资融券

---
+++ PythonApplication1/AnalysisProcessor经营分析.py
@@ -0,0 +1,25 @@
+from RClient import RClient
+import json
+from CONVERTOR import CONVERT
+from CHECKER import CHECKER 
+import jieba
+
+r=RClient.GetInst()
+
+def callback(code):  
+    #print(code)
+    keyName = "PreData:股票:%s:经营分析"%code
+    arr=  r.SortDictGetMax(keyName)
+    if [] !=arr:
+        arr = sorted(arr,  key  = lambda  k:json.loads(k)["日期Tag"],reverse=True)
+        if 0<len(arr):
+            data =  json.loads(arr[0] ) 
+            code = data["Code"]
+            name = data["Name"]
+            #dateTag = data["DateTag"]
+            print(data)
+    pass
+_keys  =r.DictKeys("AnalysisData:AllStockCode")
+for _key in _keys :
+    callback(_key)
+print("OK")
---
+++ PythonApplication1/AnalysisProcessor融资融券.py
@@ -0,0 +1,25 @@
+from RClient import RClient
+import json
+from CONVERTOR import CONVERT
+from CHECKER import CHECKER 
+import jieba
+
+r=RClient.GetInst()
+
+def callback(code):  
+    #print(code)
+    keyName = "PreData:股票:%s:融资融券"%code
+    arr=  r.SortDictGetMax(keyName)
+    if [] !=arr:
+        arr = sorted(arr,  key  = lambda  k:json.loads(k)["交易日期Tag"],reverse=True)
+        if 0<len(arr):
+            data =  json.loads(arr[0] ) 
+            code = data["Code"]
+            name = data["Name"]
+            #dateTag = data["DateTag"]
+            print(data)
+    pass
+_keys  =r.DictKeys("AnalysisData:AllStockCode")
+for _key in _keys :
+    callback(_key)
+print("OK")
